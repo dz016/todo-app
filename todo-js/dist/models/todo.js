00e4b3c858f66d21e33a7c2367fe9c492eab2a33
Dawood Zargar <dawoodzargar08@gmail.com>
2024-02-03 19:18:33 +0530

    feature addition

--- todo-js/dist/models/todo.js
+++
@@ -1,8 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-const todoSchema = new Schema({
-    title: String,
-    description: String,
-    done: Boolean,
-    userId: String,
-});
-export const Todo = mongoose.model("Todo", todoSchema);
