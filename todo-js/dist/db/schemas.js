a71b8bb19d080334f5b0aa694bb0f0646fb59d47
Dawood Zargar <dawoodzargar08@gmail.com>
2024-03-16 20:25:52 +0530

    fix dist

--- todo-js/dist/db/schemas.js
+++ todo-js/dist/db/schemas.js
@@ -1,13 +1,13 @@
 import mongoose, { Schema } from "mongoose";
 const userSchema = new Schema({
-    username: { type: String, required: true },
-    password: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
 });
 const todoSchema = new Schema({
-    title: String,
-    description: String,
-    done: Boolean,
-    userId: String,
+  title: String,
+  description: String,
+  done: Boolean,
+  userId: String,
 });
 export const User = mongoose.model("User", userSchema);
 export const Todo = mongoose.model("Todo", todoSchema);
